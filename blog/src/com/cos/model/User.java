d1fbd83a09f58025d352b114806fcc7c65729f45
woojeong <cwj2306@gmail.com>
2019-11-01 16:30:01 +0900

    댓글 기능(부분 리로딩)을 위한 Ajax 테스트

--- blog/src/com/cos/model/User.java
+++ blog/src/com/cos/model/User.java
@@ -2,6 +2,13 @@ package com.cos.model;
 
 import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 	private int id;
 //	private String userProfile;
@@ -11,64 +18,4 @@ public class User {
 	private String address;
 	private Timestamp createDate;
 	
-	public User() {
-	}
-
-	public User(int id, String username, String password, String email, String address, Timestamp createDate) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.address = address;
-		this.createDate = createDate;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-	public Timestamp getCreateDate() {
-		return createDate;
-	}
-
-	public void setCreateDate(Timestamp createDate) {
-		this.createDate = createDate;
-	}
-
 }
